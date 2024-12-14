10c1fee9e7c06915724c5ea80f438e87dd9a77ad
dipto <diptosaha.ndc50@gmail.com>
2024-12-14 17:16:45 +0600

    Project Created

---
+++ SmartCalc/SmartCalc/SmartCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCalcApp.swift
+//  SmartCalc
+//
+//  Created by Tanmoy Biswas on 14/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SmartCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
